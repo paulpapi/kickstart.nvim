b4af24a4192d40371e3c2461f6b78ee436f934b4
paulpapi <pawlos.addisu-ug@aau.edu.et>
2025-12-22 15:31:37 +0300

    rust

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,18 +1,78 @@
 return {
-  -- LSP configuration
-  { 'neovim/nvim-lspconfig' },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'williamboman/mason.nvim', build = ':MasonUpdate' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'hrsh7th/cmp-nvim-lsp' }, -- LSP → cmp bridge
+    },
 
-  -- Autocompletion engine
-  { 'hrsh7th/nvim-cmp' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
-  { 'saadparwaiz1/cmp_luasnip' },
+    config = function()
+      local lspconfig = require 'lspconfig'
 
-  -- Snippet engine
-  { 'L3MON4D3/LuaSnip' },
+      -- Capabilities for nvim-cmp
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  -- Optional: LSP installer
-  { 'williamboman/mason.nvim', build = ':MasonUpdate' },
-  { 'williamboman/mason-lspconfig.nvim' },
+      -- Mason setup
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          'rust_analyzer',
+          'tsserver',
+          'jsonls',
+          'html',
+          'cssls',
+          'lua_ls',
+        },
+      }
+
+      -- ======================
+      -- RUST (Intellisense + Clippy)
+      -- ======================
+      lspconfig.rust_analyzer.setup {
+        capabilities = capabilities,
+        settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = {
+              command = 'clippy',
+            },
+            diagnostics = {
+              enable = true,
+            },
+          },
+        },
+      }
+
+      -- ======================
+      -- JAVASCRIPT / NODE / TS
+      -- ======================
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+      }
+
+      -- ======================
+      -- LUA (Neovim config)
+      -- ======================
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      }
+
+      -- ======================
+      -- HTML / CSS / JSON
+      -- ======================
+      lspconfig.html.setup { capabilities = capabilities }
+      lspconfig.cssls.setup { capabilities = capabilities }
+      lspconfig.jsonls.setup { capabilities = capabilities }
+    end,
+  },
 }
